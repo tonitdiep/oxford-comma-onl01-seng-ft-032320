478d2f0eda0ca7591e003ad4fafcaf59d106c4e4
Toni  Diep <adiep123@gmail.com>
2020-03-27 19:22:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ binding.pry
   elsif array.count == 2   #add add in a two-element array
     return array.join("and")
   elsif array.count == 3
-    return array.join(",") + (array[-1] with ",")    #add comma and "and" in a three-element array
+    #return array.join(",") + (array[-1] with ",")    #add comma and "and" in a three-element array
   else array == 5
     return array.count()
   end
