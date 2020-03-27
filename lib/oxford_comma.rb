ac8ef7968f8e922cfe6b2e4bcd3914272b0e87de
Toni  Diep <adiep123@gmail.com>
2020-03-27 19:25:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 require 'pry'
 def oxford_comma(array)
-binding.pry
-  if array == 1
+#binding.pry
+  if array.count== 1
     return array
   elsif array.count == 2   #add add in a two-element array
     return array.join("and")
   elsif array.count == 3
     #return array.join(",") + (array[-1] with ",")    #add comma and "and" in a three-element array
-  else array == 5
+  else array.count == 5
     return array.count()
   end
 end
